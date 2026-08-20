e2dc5f4a9ebe23cdab70bef2f1d1c4a82ce68a86
nalinasai <nalinanalina995@gmail.com>
2026-08-20 23:05:31 +0530

    feat(messaging): implement analysis job messaging flow

--- src/main/java/com/debtlens/analysisservice/messaging/AnalysisJobConsumer.java
+++ src/main/java/com/debtlens/analysisservice/messaging/AnalysisJobConsumer.java
@@ -1,26 +1,25 @@
 package com.debtlens.analysisservice.messaging;
 
+import com.debtlens.analysisservice.config.RabbitMQConfig;
+import com.debtlens.analysisservice.dto.AnalysisJobMessage;
+import com.debtlens.analysisservice.dto.AnalysisResult;
+import com.debtlens.analysisservice.service.AnalysisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class AnalysisJobConsumer {
+
+    private final AnalysisService analysisService;
+    private final AnalysisResultPublisher analysisResultPublisher;
+
+    @RabbitListener(queues = RabbitMQConfig.ANALYSIS_JOB_QUEUE)
+    public void consume(AnalysisJobMessage job) {
+
+        AnalysisResult result = analysisService.analyze(job);
+
+        analysisResultPublisher.publish(result);
+    }
 }
-/*AnalysisJobConsumer
-
-Consumes:
-
-Application Backend
-       ↓
-RabbitMQ
-       ↓
-AnalysisJobConsumer
-
-It receives something like:
-
-analysisJobId
-repositoryId
-repositoryUrl
-branch
-
-Then it calls:
-
-AnalysisService
-
-It should not contain the actual analysis logic.*/
--- src/main/java/com/debtlens/analysisservice/messaging/AnalysisResultPublisher.java
+++ src/main/java/com/debtlens/analysisservice/messaging/AnalysisResultPublisher.java
@@ -1,6 +1,23 @@
 package com.debtlens.analysisservice.messaging;
 
+import com.debtlens.analysisservice.config.RabbitMQConfig;
+import com.debtlens.analysisservice.dto.AnalysisResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class AnalysisResultPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void publish(AnalysisResult result) {
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.ANALYSIS_RESULT_QUEUE,
+                result
+        );
+    }
 }
 /*AnalysisResultPublisher
 
